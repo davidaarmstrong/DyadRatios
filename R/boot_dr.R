0f141dd404ed90da79552351b7db66cdd2c3ffda
Dave Armstrong <dave@quantoid.net>
2025-10-09 14:26:35 -0400

    Updated bootstrapping algorithm and vignette

--- R/boot_dr.R
+++ R/boot_dr.R
@@ -31,14 +31,23 @@
 #' @param endmonth Ending month of the analysis.
 #' @param R Number of bootstrap samples.
 #' @param parallel Logical indicating whether the `mclapply` function should be used. 
+#' @param level The confidence level for the intervals. Default is 0.95. 
+#' @param pw Logical indicating whether to do pairwise tests. 
 #' @param ... Other arguments to be passed down to `mclapply`.  
 #'
-#' @return A data frame with variables:
+#' @return A list with potentially two data frames `ci` has variables:
 #' \itemize{
 #'   \item \code{period}: Aggregation period. 
 #'   \item \code{latent1}: Estimate of latent variable from original analysis.
 #'   \item \code{lwr}: Lower confidence bound.
 #'   \item \code{upr}: Upper confidence bound. 
+#' }
+#' If `pw = TRUE`, the list also contains `pw` with variables:
+#' \itemize{
+#'   \item \code{p1}: Earlier period 
+#'   \item \code{p2}: Later period 
+#'   \item \code{diff}: (mood for p2) - (mood for p1)
+#'   \item \code{p_diff}: Probability that the larger mood is bigger than the smaller mood. 
 #' }
 #'
 #' @references
@@ -48,10 +57,11 @@
 #' \emph{Bulletin of Sociological Methodology/Bulletin de Méthodologie
 #' Sociologique}, 137–138(1), 201–218. \doi{10.1177/0759106318761614}
 #'
-#' @importFrom stats cor lm optim sd var quantile aggregate rbinom
+#' @importFrom stats cor lm optim sd var quantile aggregate rbinom reshape
 #' @importFrom graphics legend lines par plot 
 #' @importFrom lubridate ymd year month day quarter
 #' @importFrom progress progress_bar
+#' @importFrom utils combn
 #' 
 #' @usage NULL
 #' @export
@@ -82,6 +92,8 @@ boot_dr <- function(varname,
                     endmonth=12, 
                     R=1000, 
                     parallel=FALSE, 
+                    level = .95,
+                    pw = FALSE, 
                     ...) { 
   if(is.null(ncases))ncases <- rep(1000, length(index))
   if(!is.null(ncases) & any(is.na(ncases)))ncases <- ifelse(is.na(ncases), 1000, ncases)
@@ -93,8 +105,10 @@ boot_dr <- function(varname,
     for(iter in 1:R){
       n_cases_boot <- rbinom(n = length(index), prob= index, size=ncases)
       index_boot <- n_cases_boot/ncases
-      e <- DyadRatios::extract(varname, date, index_boot, ncases, unit, mult, begindt, enddt, npass, smoothing, endmonth)
-      moods[[iter]] <- get_mood(e)
+      e <- extract_bs(varname, date, index_boot, ncases, unit, mult, begindt, enddt, npass, smoothing, endmonth, wm=e_orig$wtdmean, ws=e_orig$wtdstd)
+      out <- get_mood(e)
+      out$iter <- iter
+      moods[[iter]] <- out
       pb$tick()
     }
   }else{
@@ -104,25 +118,40 @@ boot_dr <- function(varname,
     moods <- parallel::mclapply(1:R, \(i){
       n_cases_boot <- rbinom(n = length(index), prob= index, size=ncases)
       index_boot <- n_cases_boot/ncases
-      e <- DyadRatios::extract(varname, date, index_boot, ncases, unit, mult, begindt, enddt, npass, smoothing, endmonth)
-      get_mood(e)
+      e <- extract_bs(varname, date, index_boot, ncases, unit, mult, begindt, enddt, npass, smoothing, endmonth, wm=e_orig$wtdmean, ws=e_orig$wtdstd)
+      out <- get_mood(e)
+      out$iter <- i
+      out
     }, ...)
     
   }
-  make_qtl <- function(x){
-    data.frame(lwr = c(unname(quantile(x, .025))), 
-               upr = c(unname(quantile(x, .975))))
-  }
+  lwr_level <- (1 - level)/2
+  upr_level <- 1 - lwr_level
   moods <- as.data.frame(do.call(rbind, moods))
+  if(pw){
+    moodsw <- reshape(moods, idvar = "period", timevar = "iter", direction = "wide")
+    pd <- moodsw$period
+    moods_mat <- t(as.matrix(moodsw[,-1]))
+    combs <- combn(ncol(moods_mat), 2)
+    D <- matrix(0, nrow=ncol(moods_mat), ncol=ncol(combs))
+    D[cbind(combs[1,], 1:ncol(combs))] <- -1
+    D[cbind(combs[2,], 1:ncol(combs))] <- 1
+    diffs <- moods_mat %*% D
+    p_diff <- apply(diffs, 2, \(x)mean(x > 0))
+    p_diff <- ifelse(p_diff < .5, 1-p_diff, p_diff)
+    diff = matrix(get_mood(e_orig)$latent1, nrow=1) %*% D
+    res_pw <- data.frame(p1 = pd[combs[1,]], p2 = pd[combs[2,]], diff = c(diff), p_diff = p_diff)
+  }
   lwr <- stats::aggregate(moods$latent1, list(moods$period), 
-                   \(x)c(unname(quantile(x, .025))))
+                   \(x)c(unname(quantile(x, lwr_level))))
   upr <- stats::aggregate(moods$latent1, list(moods$period), 
-                   \(x)c(unname(quantile(x, .975))))
+                   \(x)c(unname(quantile(x, upr_level))))
   names(lwr) <- c("period", "lwr")
   names(upr) <- c("period", "upr")
   cis <- merge(lwr, upr)
-  res <- merge(get_mood(e_orig), cis)
-  class(res) <- c("boot_dr", "data.frame")
-  res
+  cis <- merge(get_mood(e_orig), cis)
+  res <- list(ci = cis)
+  if(pw)res[["pw"]] <- res_pw
+  return(res)
 }
 
--- R/extract.R
+++ R/extract.R
@@ -58,6 +58,7 @@
 #'   \item \code{var_exp}: Data frame with variance explained and proportion of variance explained for each dimension.
 #'   \item \code{dropped}: Data frame listing any series that were dropped from the analysis due to insufficient data.
 #'   \item \code{smoothing}: Logical indicating whether smoothing was applied during the iterative solution process.
+#'   \item \code{ms_list}: List of means and variances for standardizing the input series for the bootstrap. 
 #' }
 #'
 #' @references
@@ -266,7 +267,7 @@ extract <- function(varname,date,index,ncases=NULL,unit="A",mult=1,begindt=NA,en
 
 #export<<-list(nperiods,nvar,issue)
 
-
+  ms_list <- vector(mode = "list", length=npass)
   for (pass in 1:npass) { #newpass: RESTART FOR SECOND DIMENSION CASE
     if (pass == 2) { #reset iteration control parameters
       iter <- 0
@@ -285,6 +286,7 @@ extract <- function(varname,date,index,ncases=NULL,unit="A",mult=1,begindt=NA,en
         issue[,v]<- 100 + 10 * (issue[,v] - av[v])/std[v]  #standardize
 #        ngood<- ngood+1
       }#end for
+    ms_list[[pass]] <- list(av=av, std=std)
     }
     #READY FOR ESTIMATION, SET UP AND PRINT OPTIONS INFO     
     out<- as.character(10) #initial length only
@@ -454,7 +456,8 @@ extract <- function(varname,date,index,ncases=NULL,unit="A",mult=1,begindt=NA,en
                          totalvar = totalvar1, 
                          var_exp = var_exp, 
                          dropped=dropped, 
-                         smoothing = smoothing)
+                         smoothing = smoothing, 
+                         ms_list = ms_list)
     } else {
     extract.out<- list(call=formula,
                        T=nperiods,
@@ -478,7 +481,8 @@ extract <- function(varname,date,index,ncases=NULL,unit="A",mult=1,begindt=NA,en
                        totalvar = totalvar1, 
                        var_exp = var_exp, 
                        dropped=dropped, 
-                       smoothing = smoothing)
+                       smoothing = smoothing, 
+                       ms_list = ms_list)
     }
     } #end if auto="y" 
   } #end of for pass=1,2 loop 
@@ -488,6 +492,413 @@ extract <- function(varname,date,index,ncases=NULL,unit="A",mult=1,begindt=NA,en
   return(extract.out)
   }
 
+#' Extract for bootstrap includes arguments for means and standard deviations for standardizing
+#' @noRd
+extract_bs <- function(varname,date,index,ncases=NULL,unit="A",mult=1,begindt=NA,enddt=NA,npass=1,smoothing=TRUE,endmonth=12, wm, ws) {
+  tmp <- data.frame(varname = varname, date = date, index = index, ncases = ncases)
+  sp_tmp <- split(tmp, tmp$varname)
+  for(i in seq_along(sp_tmp)) {
+    sp_tmp[[i]] <- sp_tmp[[i]][order(sp_tmp[[i]]$date), ]
+  }
+  tmp <- do.call(rbind, sp_tmp)
+  varname <- tmp$varname
+  index <- tmp$index
+  date <- tmp$date
+  ncases <- tmp$ncases
+  formula<-match.call(extract_bs)
+  csign <- NULL
+  nrecords<- length(varname)
+  if (is.null(ncases)) ncases<- rep(0,nrecords)
+  moddate <- date #create temporary date vector, leaving original unmodified
+  if ((unit=="A" || unit=="O") && endmonth<12) {
+    for (i in 1:nrecords) { #first loop through raw data file
+      month<- month(moddate[i])
+      year<- year(moddate[i])
+      if (month>endmonth) moddate[i]<- ymd(paste(year, "01", "01", sep="-"), tz="GMT") #modified date become 1/1 of next year
+    } #end loop through data
+  } # end if
+  
+  if (is.na(begindt)) minper<-month(min(moddate)) else minper<-month(begindt)
+  if (is.na(begindt)) miny<-year(min(moddate)) else miny<-year(begindt)
+  if (is.na(begindt)) minday<-day(min(moddate)) else minday<-day(begindt)
+  if (is.na(enddt)) maxper<-month(max(moddate)) else maxper<-month(enddt)
+  if (is.na(enddt)) maxy<-year(max(moddate)) else maxy<-year(enddt)
+  if (is.na(enddt)) maxday<-day(max(moddate)) else maxday<-day(enddt)
+  if (unit=="Q") {
+    minper<- as.integer((minper-1)/3)+1
+    maxper<- as.integer((maxper-1)/3)+1
+  }
+  # mindate<- ISOdate(miny,minper,minday,0,0,0,tz="GMT")
+  # maxdate<- ISOdate(maxy, maxper, maxday,0,0,0,tz="GMT") #86400=24*60*60
+  
+  mindate <- lubridate::ymd(paste(miny, minper, minday, sep="-"), tz="GMT")
+  maxdate <- lubridate::ymd(paste(maxy, maxper, maxday, sep="-"), tz="GMT")
+  
+  
+  #SETCONS:
+  latent<- numeric(1)
+  aggratio<- 0
+  fb<- 1 #initialize
+  auto<- "start"  #meaningless value
+  alpha<- 1
+  alpha1<- 1
+  pass<- 1
+  holdtola<- 0.001
+  tola<- holdtola
+  iter<- 0
+  lastconv<- 99999
+  wtmean<- 0 #for it=1
+  wtstd<- 1
+  fract<- 1
+  iter_hist <- NULL
+  dropped <- NULL
+  if (unit=="A") {
+    nperiods<- maxy-miny+1
+    aggratio<- 1
+    months<- 12
+  }
+  if (unit=="O") {
+    years<- mult
+    months<- years*12
+    aggratio<- 2
+    odd<- (maxy-miny+1) %% mult  #mod
+    nperiods=as.integer((maxy-miny)/mult) + odd
+  }
+  if (unit=="M") {
+    fract<- 100
+    nperiods<- (maxy-miny)*12
+    nperiods<- nperiods-12 + (12-minper+1) + maxper
+    aggratio<- 1/12
+    months<- 1
+  }
+  if (unit=="Q") {
+    aggratio<- 1/4
+    months<- 3
+    nperiods<- as.integer((maxy-miny)/aggratio)
+    nperiods<- nperiods-4 + (4-minper+1) + maxper
+    fract<- 10
+  }
+  if (unit=="D") {
+    months=1
+    nperiods<- (as.integer(maxdate)-as.integer(mindate)) + 1 
+  }
+  
+  arinv<- 1/aggratio
+  aggratio<- months/12
+  nrecords<- length(index)
+  
+  #HERE WE SET UP FUNDAMENTAL DIMENSIONS AND DECLARE VECTORS
+  if (fb != 2)  mood<- array(dim=c(3,nperiods))
+  vfac<- factor(varname) #make a factor vector
+  vlev<- levels(vfac)    #find unique categories
+  nvar<- length(vlev)    #how many are there?, includes unusable series
+  valid<- numeric(nvar)
+  csign<- numeric(nvar)
+  assign("csign", numeric(nvar), envir=parent.frame())
+  vl<- character(nvar)
+  r<- numeric(nvar)
+  oldr<- rep(1,nvar) # r=1 for all v initially
+  
+  issue<- array(dim=c(nperiods,nvar))
+  count<- numeric(nperiods)
+  vl<- numeric(nvar)
+  period<- numeric(nperiods)
+  converge<- 0
+  evalue<- 0
+  evalue1 <- 0
+  evalue2 <- NULL
+  # create numeric variable period, eg, yyyy.0m 
+  if (unit=="D") {
+    period<-seq(1:nperiods) 
+  } else {
+    if (months >= 12) {
+      for (l in 1:nperiods) { 
+        p <- (l - 1) * aggratio
+        period[l] <- miny + p
+      } #next l
+    } else {
+      y <- 0
+      i <- 0
+      my <- miny
+      if (minper == 1)  my <- my - 1
+      for (l in 1:nperiods) { 
+        i<- 1 + ((l-1) %% arinv)
+        mq <- minper + i - 1
+        mq<- 1 + ((mq-1) %% arinv) 
+        if (mq == 1)  y <- y + 1 #first month or quarter, increment year
+        period[l] <- my + y + mq / fract
+      } # end for
+    } #end else
+  } # end if
+  
+  
+  agglist<- aggregate(varname,moddate,index,ncases,mindate,maxdate,nperiods,nvar,aggratio,unit,miny,minper) # call aggregate to produce issue matrix
+  vl<- agglist$lab #extract two elements of the list from aggregate call
+  issue<- agglist$iss
+  rm(agglist) #don't need this anymore
+  
+  #NOW REDUCE ISSUE MATRIX TO ELIMINATE UNUSABLE SERIES (WN<2)
+  ndrop<- 0
+  nissue<- numeric(nperiods)
+  std<- numeric(nperiods)
+  for (v in 1:nvar) {
+    std[v]<- 0 #default
+    nissue[v]<- sum(!is.na(issue[,v])) #criterion is 2 cases for npass=1 or 3 for npass=2
+    if (nissue[v]>npass) std[v]<- sqrt(var(issue[,v],na.rm=TRUE)) #this is just a test for variance >0
+    if (std[v]<.001) {  #case dropped if std uncomputable (NA) or actually zero (constant)
+      ndrop<- ndrop+1
+      dropped <- rbind(dropped, data.frame(Dropped = vl[v], N_remain = nissue[v]))
+    }
+  }
+  nvarold<- nvar
+  nvar<- nvar-ndrop
+  pointer<- 1
+  found<- FALSE
+  
+  for (v in 1:nvar) { #now reduced nvar
+    while (found==FALSE && pointer<=nvarold) { #find first valid column and push down
+      if (std[pointer]>.001) { #good case, transfer
+        issue[,v]<- issue[,pointer]
+        vl[v]<- vl[pointer]
+        pointer<- pointer+1
+        found<- TRUE
+      } else {
+        pointer<- pointer+1 #bad case, increment pointer
+      } #end if
+    } #end while
+    found<- FALSE
+  } #for
+  length(vl)<- nvar #reduce  
+  length(issue)<- nperiods*nvar  #chop off unused columns
+  attr(issue,"dim")<- c(nperiods,nvar)
+  N<- numeric(nvar)
+  
+  #export<<-list(nperiods,nvar,issue)
+  
+  ms_list <- vector(mode = "list", length=npass)
+  for (pass in 1:npass) { #newpass: RESTART FOR SECOND DIMENSION CASE
+    if (pass == 2) { #reset iteration control parameters
+      iter <- 0
+      tola = holdtola
+      lastconv <- 99999
+      converge<- lastconv
+      conv<- converge
+    } else {
+      av<- numeric(nvar)
+      std<- numeric(nvar)
+      #      ngood<- 0
+      for (v in 1:nvar) { #compute av and std by issue nvar now reduced to good cases
+        wn<- as.integer(nperiods-sum(is.na(issue[,v])))
+        av[v] <- mean(issue[,v],na.rm=TRUE)
+        std[v]<- sqrt(var(issue[,v],na.rm=TRUE) * ((wn - 1)/wn)) #convert to population standard deviation
+        issue[,v]<- 100 + 10 * (issue[,v] - av[v])/std[v]  #standardize
+        #        ngood<- ngood+1
+      }#end for
+      ms_list[[pass]] <- list(av=av, std=std)
+    }
+    #READY FOR ESTIMATION, SET UP AND PRINT OPTIONS INFO     
+    out<- as.character(10) #initial length only
+    setup <- list(start = miny, end = maxy, nperiods = nperiods, used_series = nvar, unused_series = ndrop, smoothiing=smoothing)
+    if(!is.null(minper)){
+      setup$minper <- minper
+      setup$maxper <- maxper
+    }
+    for (p in 1:nperiods) {
+      count[p]<- sum(!is.na(issue[p,]))
+    }
+    valid<- rep(1,times=nvar)
+    csign<- rep(1,times=nvar)
+    assign("csign", rep(1,times=nvar), envir=parent.frame())
+    auto <- "y"              #iterative estimation on by default
+    quit <- 0                #false implies go ahead and estimate
+    
+    while (iter == 0 || converge > tola) {   #MASTER CONTROL LOOP WHICH ITERATES UNTIL SOLUTION REACHED
+      
+      for (fb in 1:2) { #    MASTER fb LOOP       fb=1 is forward, 2 backward 
+        dominate.out<- dominate(fb,issue,nperiods,nvar,mood,valid,smoothing,alpha, csign)  #master estimation routine
+        alpha1<- dominate.out$alpha1
+        alpha<- dominate.out$alpha
+        mood[fb,]<- dominate.out$latent
+      } #next fb
+      
+      fb <- 3 #average mood from here on
+      for (p in 1:nperiods) { #    AVERAGE
+        mood[fb, p] <- (mood[1, p] + mood[2, p]) / 2
+      } #next p
+      moodmean<-mean(mood[3,])
+      sdmood<-sd(mood[3,])
+      for (p in 1:nperiods) {  #PLACEMENT OF THIS LOOP MAY NOT BE RIGHT
+        mood[fb,p] <- ((mood[fb,p] - moodmean) * ws / sdmood) + wm
+      } #end for
+      
+      iter <- iter + 1 
+      if (auto == "y") r<- iscorr(issue,mood) else auto <- "y"   #recompute correlations
+      
+      wtmean<- 0
+      wtstd<- 0
+      vsum<- 0
+      goodvar<- 0
+      converge<- 0 #start off default
+      evalue<- 0
+      totalvar<- 0
+      
+      assign("csign", ifelse(is.na(sign(r)), csign, sign(r)), envir=parent.frame())
+      for (v in 1:nvar) {
+        wn<- nperiods-sum(is.na(issue[,v]))
+        #if (!is.na(sign(r[v]))) csign[v]<<- sign(r[v])
+        wn<- nperiods-sum(is.na(issue[,v]))
+        if (wn>1) { #sum over variables actually used
+          vratio <- wn / nperiods
+          evalue <- evalue + vratio * r[v]^2
+          totalvar <- totalvar + vratio
+        } #end if
+        evalue1 <- evalue
+        totalvar1 <- totalvar
+        #convergence tests
+        if (wn > 3) {
+          conv <- abs(r[v] - oldr[v])      #conv is convergence test for item=v
+          conv <- conv * (wn / nperiods)                #weight criterion by number of available periods
+          if (conv > converge)  converge <- conv        #converge is the global max of conv
+        } #end if
+        if (!is.na(r[v])) oldr[v] <- r[v]
+        if (!is.na(r[v])) valid[v] <- r[v]^2
+        if (!is.na(av[v])) wtmean <- wtmean + av[v] * valid[v]
+        if (!is.na(std[v])) wtstd <- wtstd + std[v] * valid[v]
+        if (!is.na(r[v])) vsum <- vsum + valid[v]
+      } #end v loop
+      
+      if (vsum > 0)  wtmean <- wtmean / vsum
+      if (vsum > 0)  wtstd <- wtstd / vsum
+      if (pass == 1) {
+        mean1 <- wtmean
+        std1 <- wtstd
+        e1=evalue
+      } else {
+        wtmean <- mean1
+        wtstd <- std1 #*unexp
+      } #end if
+      fbcorr <- cor(mood[1,],mood[2,]) #fnfrontback 
+      if (quit != 1) {
+        cv<- format(round(converge,4),nsmall=4) 
+        itfmt<-format(round(iter),justify="right",length=4)
+        iter_hist <- rbind(iter_hist, data.frame(Dimension = paste0("Dimension ", pass), Iter = itfmt, Convergence = cv, Criterion = tola, 
+                                                 Reliability = fbcorr, Alphaf = alpha1, Alphab = alpha))
+      }
+      if (converge > lastconv)  tola <- tola * 2
+      lastconv <- converge
+      
+      auto = "y"  #skip corr on iter=1, set auto on
+      
+      if (iter >= 50) break #get out of while loop
+    } #END MASTER WHILE ITERATION CONTROL LOOP
+    
+    if (auto == "y" && converge<tola) { #IF WE REACH THIS CODE WE HAVE A FINAL SOLUTION TO BE REPORTED
+      if (pass == 1) out1<- out #hold output for 2 dimensional solution
+      auto <- "Q"
+      quit <- 1                #flag solution reached, last time through
+      r<- iscorr(issue,mood)   #final iteration correlations
+      if (pass == 1) r1<- r #hold correlations for 2 dimensional solution
+      
+      if (pass > 1) {
+        unexp <- totalvar 
+        totalvar <- unexp * totalvar
+        evalue <- evalue * unexp
+      } #    end if
+      
+      if (pass == 1) {
+        expprop <- evalue / totalvar
+        tot1 <- totalvar
+        tot_var <- tot1
+        expprop1 <- expprop
+      } else {
+        erel <- evalue / totalvar          #% exp relative
+        totalvar <- (1 - expprop) * tot1   #true var=original var discounted by %exp
+        evalue <- erel * totalvar          #rescale to retain %exp relationship
+        expprop <- evalue / tot1           #now reduce eral to expprop
+        evalue2 <- evalue
+        expprop2 <- expprop
+      } #    end if
+      
+      for (v in 1:nvar) {
+        N[v]<- sum(!is.na(issue[,v]))
+      }
+      var.out<- list(varname=vl,loadings=r,means=av,std.deviations=std)
+      if (pass !=  2 && npass>1) {
+        for (v in 1:nvar) { 
+          valid[v] <- 0               #reset all, regmoodissue will set good=1
+          if (csign[v] != 0)  issue[,v]<- residmi(issue,v,mood)   #regmoodissue()
+        } #v loop
+      }  # if
+      #begin prn output routine # mood[fb,] is now our estimate,    WHAT ABOUT A SECOND DIMENSION
+      
+      var_exp <- data.frame(Dimension =  "Dimension 1", 
+                            Variance = evalue1, 
+                            Proportion = expprop1)
+      if(!is.null(evalue2)){
+        var_exp <- rbind(var_exp, 
+                         data.frame(Dimension = "Dimension 2", 
+                                    Variance = evalue2, 
+                                    Proportion = expprop2))
+      }
+      
+      latent<- mood[fb,] #vector holds values for output
+      if (pass == 1) latent1<- latent #hold first dimension
+      #for Zelig output
+      if (npass==1) {
+        extract.out<- list(call=formula,
+                           T=nperiods,
+                           nvar=nvar,
+                           unit=unit,
+                           dimensions=npass,
+                           period=period,
+                           varname=vl,
+                           N=N,
+                           wtdmean=wtmean,
+                           wtdstd=wtstd,
+                           means=av,
+                           std.deviations=std,
+                           setup1=out1,
+                           loadings1=r1,
+                           latent1=latent1, 
+                           hist = iter_hist, 
+                           totalvar = totalvar1, 
+                           var_exp = var_exp, 
+                           dropped=dropped, 
+                           smoothing = smoothing, 
+                           ms_list = ms_list)
+      } else {
+        extract.out<- list(call=formula,
+                           T=nperiods,
+                           nvar=nvar,
+                           unit=unit,
+                           dimensions=npass,
+                           period=period,
+                           varname=vl,
+                           N=N,
+                           wtdmean=wtmean,
+                           wtdstd=wtstd,
+                           means=av,
+                           std.deviations=std,
+                           setup1=out1,
+                           loadings1=r1,
+                           latent1=latent1,
+                           setup2=out,
+                           loadings2=r,
+                           latent2=latent, 
+                           hist = iter_hist,
+                           totalvar = totalvar1, 
+                           var_exp = var_exp, 
+                           dropped=dropped, 
+                           smoothing = smoothing, 
+                           ms_list = ms_list)
+      }
+    } #end if auto="y" 
+  } #end of for pass=1,2 loop 
+  
+  # par(col=1) #reset on termination
+  class(extract.out)<- "extract"
+  return(extract.out)
+}
 
 
 
